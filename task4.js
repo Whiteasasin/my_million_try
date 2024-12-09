4d28b2481c591898be054f7f3d3d992afa384eb8
Олексій Педенко <oleksijpedenko@MacBook-Pro-Oleksij.local>
2024-12-10 01:51:47 +0200

    fourth task

---
+++ task4.js
@@ -0,0 +1,27 @@
+// Завдання 4: Обчислення площі та об'єму
+
+let radius = 3 ;
+
+let sKola = Math.PI * (radius * radius);
+let sKolaPow = Math.PI *(Math.pow(radius, 2));
+
+console.log(sKola.toFixed(2));
+console.log(sKolaPow.toFixed(2));
+
+// Завдання 4.2
+
+let length = 12 ; 
+let width = 13 ;
+
+let sSquar = length * width ; 
+
+console.log(sSquar);
+
+//Завдання 4.3
+
+let radCil = 12 ;
+let height = 12 ;
+
+let resultCil = Math.PI * (Math.pow(radius, 2)) * height ;
+
+console.log(resultCil.toFixed(2));
