e21210f3bd7d0dc34dd8938df0ba60ce057dbaac
Олексій Педенко <oleksijpedenko@MacBook-Pro-Oleksij.local>
2024-12-19 18:13:12 +0200

    task2 added

---
+++ hwjs6/task2.js
@@ -0,0 +1,13 @@
+
+
+function isAdult(age) {
+    return age >= 18;
+}
+
+
+const outp1 = isAdult(25); 
+const outp2 = isAdult(15); 
+
+
+console.log("Чи є особа з віком 25 дорослою?", outp1); 
+console.log("Чи є особа з віком 15 дорослою?", outp2); 
